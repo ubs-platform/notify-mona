5d59d7bde3e227b6ccf6eb1fdf5a136d133ad5ca
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2024-03-11 19:13:44 +0300

    config

--- src/app/controller/base/base-crud.controller.ts
+++ src/app/controller/base/base-crud.controller.ts
@@ -13,31 +13,85 @@ import {
 import { Roles, RolesGuard } from '@ubs-platform/users-mona-roles';
 import { JwtAuthGuard } from '@ubs-platform/users-mona-microservice-helper';
 import { IBaseCrudService } from 'src/app/service/base/base-crud.service';
+import {
+  ControllerConfiguration,
+  RoleAuthorization,
+  RoleAuthorizationConfigKey,
+  RoleAuthorizationDetailed,
+} from './controller-configuration';
+import { ROUTES } from '@nestjs/core/router/router-module';
 
 export const BaseCrudControllerGenerator = <
   MODEL,
   INPUT extends { _id },
   OUTPUT
->() => {
+>(
+  r?: ControllerConfiguration
+) => {
+  const findExistAuth = (field: RoleAuthorizationConfigKey) => {
+    let a = r.authorization[field] || r.authorization.ALL;
+    if (a === true) {
+      return {
+        needsAuthenticated: true,
+        roles: [],
+      } as RoleAuthorizationDetailed;
+    } else if (!a) {
+      return {
+        needsAuthenticated: false,
+        roles: [],
+      } as RoleAuthorizationDetailed;
+    } else {
+      return a as RoleAuthorizationDetailed;
+    }
+  };
+
+  const RoleConfig = (field: RoleAuthorizationConfigKey) => {
+    let authorization = findExistAuth(field);
+    // if (meta == null) meta = {};
+    const guard = UseGuards(
+      authorization.needsAuthenticated ? JwtAuthGuard : null,
+      authorization.roles ? RolesGuard : null
+    );
+    const roles = Roles(authorization.roles);
+
+    return (target, propKey, descriptor) => {
+      guard(target, propKey, descriptor);
+      roles(target, propKey, descriptor);
+    };
+  };
+
+  // let roles = [];
+  // let guard = [];
+  // if (r === true) {
+  // } else if (r != null) {
+
+  // } else {
+
+  // }
   class ControllerClass {
     constructor(private service: IBaseCrudService<MODEL, INPUT, OUTPUT>) {}
 
-    @UseGuards()
+    // @UseGuards(...generateGuardAndRolesEtc('GETALL')[0])
+    // @Roles(generateGuardAndRolesEtc('GETALL')[1])
+    @RoleConfig('GETALL')
     @Get()
     async fetchAll() {
       return await this.service.fetchAll();
     }
 
     @Get('/:id')
+    @RoleConfig('GETID')
     async fetchOne(@Param() { id }: { id: any }) {
       return await this.service.fetchOne(id);
     }
 
+    @RoleConfig('ADD')
     @Post()
     async add(@Body() body: INPUT) {
       return await this.service.create(body);
     }
 
+    @RoleConfig('EDIT')
     @Put()
     async edit(@Body() body: INPUT) {
       if (body._id == null) {
@@ -47,6 +101,7 @@ export const BaseCrudControllerGenerator = <
     }
 
     @Delete(':id')
+    @RoleConfig('REMOVE')
     async remove(@Param() { id }: { id: any }) {}
   }
 
--- src/app/controller/email-template.controller.ts
+++ src/app/controller/email-template.controller.ts
@@ -4,12 +4,15 @@ import { EmailTemplate } from '../model/email-template.model';
 import { EmailTemplateDTO } from '../dto/email-template.dto';
 import { EmailTemplateService } from '../service/email-template.service';
 
+const config = {
+  authorization: { ALL: { needsAuthenticated: true, roles: ['ADMIN'] } },
+};
 @Controller('email-template')
 export class EmailTemplateController extends BaseCrudControllerGenerator<
   EmailTemplate,
   EmailTemplateDTO,
   EmailTemplateDTO
->() {
+>(config) {
   constructor(service: EmailTemplateService) {
     super(service);
   }
